d04943c469a62ce675cd5091e921354a9d062f61
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-05-02 14:17:55 +0545

    formik and yup package installed, shared folder created and made it's absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
 			'@Svg': path.resolve(__dirname, './src/assets/svg'),
 			'@Components': path.resolve(__dirname, './src/common/components'),
 			'@Utils': path.resolve(__dirname, './src/common/utils'),
+			'@Shared': path.resolve(__dirname, './src/common/shared'),
 			'@Features': path.resolve(__dirname, './src/features'),
 			'@Slices': path.resolve(__dirname, './src/redux/slices'),
 			'@Store': path.resolve(__dirname, './src/redux/store.ts'),
