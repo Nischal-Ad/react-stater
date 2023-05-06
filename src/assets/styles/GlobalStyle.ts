68d8f2f7b0cc5a319e1991191940c836d1711378
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-05-06 14:32:27 +0545

    hide scrollbar and number-input spin box

--- src/assets/styles/GlobalStyle.ts
+++ src/assets/styles/GlobalStyle.ts
@@ -12,6 +12,18 @@ const globalCss = css`
 		min-height: var(--height);
 	}
 
+	body::-webkit-scrollbar {
+		display: none;
+		-ms-overflow-style: none;
+		scrollbar-width: none;
+	}
+
+	input[type='number']::-webkit-inner-spin-button,
+	input[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none !important;
+		margin: 0 !important;
+	}
+
 	* {
 		font-family: var(--ff-poppins) !important;
 	}
