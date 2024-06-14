d56b33390418f08356f585f87672e58c075623c9
Nischal Adhikari <92177440+Nischal-Ad@users.noreply.github.com>
2024-06-14 10:52:40 +0545

    fix

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -8,9 +8,7 @@ interface ISection extends PropsWithChildren {
 const Section: React.FC<ISection> = ({ id, children, className }) => {
   return (
     <section id={id} className={`${className} container mx-auto`}>
-      <div className="2xl:mx-28 lg:mx-4 mx-2 flex flex-col space-y-6 lg:space-y-0 lg:flex-row justify-between items-center space-x-4">
-        {children}
-      </div>
+      <div className="2xl:mx-28 lg:mx-4 mx-2">{children}</div>
     </section>
   )
 }
--- src/utils/alerts.tsx
+++ src/utils/alerts.tsx
@@ -1,4 +1,5 @@
-import { message } from 'antd'
+import { message, Modal } from 'antd'
+import { MdError } from 'react-icons/md'
 
 export const notifySuccess = (content: string) => {
   message.success({ content, duration: 1 })
@@ -6,4 +7,16 @@ export const notifySuccess = (content: string) => {
 
 export const notifyError = (content: string) => {
   message.error({ content, duration: 1 })
+}
+
+export const notifyErrorDialog = (message: string) => {
+  Modal.confirm({
+    centered: true,
+    title: <p className="ml-1 text-sm">Failed</p>,
+    icon: <MdError className=" text-xl text-red-500" />,
+    cancelButtonProps: { className: 'hidden' },
+    content: message,
+    okText: 'ok',
+    okButtonProps: { className: 'bg-red-500  hover:!bg-red-500' },
+  })
 }
